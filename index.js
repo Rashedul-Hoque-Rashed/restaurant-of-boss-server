0b0d5260265428f45c0d70913b9798694eb25160
Rashed <rashedulhoqueceo@gmail.com>
2023-11-18 01:43:10 +0600

    18/11/23

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ async function run() {
 
 
     const verifyToken = (req, res, next) => {
-      console.log(req.headers)
       if (!req.headers.authorization) {
         return res.status(401).send({ message: 'unauthorize access' })
       }
@@ -58,7 +57,6 @@ async function run() {
       const query = { email: email };
       const user = await userCollections.findOne(query);
       const isAdmin = user?.role === 'admin' ? true : false;
-      console.log('for verify admin', isAdmin)
       next();
     }
 
@@ -68,9 +66,39 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/menu/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollections.findOne(query);
+      res.send(result);
+    })
+
     app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
       const item = req.body;
       const result = await menuCollections.insertOne(item);
+      res.send(result)
+    })
+
+    app.patch('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const update = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe
+        }
+      };
+      const result = await menuCollections.updateOne(filter, update);
+      res.send(result);
+    })
+
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollections.deleteOne(query);
       res.send(result)
     })
 
@@ -101,7 +129,6 @@ async function run() {
       const user = await userCollections.findOne(query);
 
       const isAdmin = (user?.role === 'admin' ? true : false);
-      console.log(isAdmin)
       res.send({ isAdmin })
     })
 
